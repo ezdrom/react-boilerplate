cd25f9bceee7ba74b13f58d756c890a5fad4dd85
ezdwaine <ezdromero@gmail.com>
2023-05-10 08:00:43 +0800

    add pokemon list with images

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,38 @@
 import { useState, useEffect } from 'react';
-import getAllPokemon from './api/PokeRequests';
+import axios from "axios";
 
 interface Pokemon {
   name: string;
   url: string;
+  sprites: {
+    front_default: string;
+  };
 }
 
 function App() {
-  const [pokemon, setPokemon] = useState<Pokemon[]>([]);
-
+  const [pokemon, setPokemon] = useState<Pokemon[] | null>(null);
   useEffect(() => {
-    async function fetchPokemon() {
-      const allPokemon = await getAllPokemon();
-      setPokemon(allPokemon);
+    async function fetchData() {
+      const urls = Array.from({ length: 20 }, (_, i) => `https://pokeapi.co/api/v2/pokemon/${i + 1}`);
+      const requests = urls.map(url => axios.get<Pokemon>(url));
+      const responses = await axios.all(requests);
+      const data = responses.map(response => response.data);
+      setPokemon(data);
     }
-    fetchPokemon();
+    fetchData();
   }, []);
-
+  if (!pokemon) {
+    return <div>Loading...</div>;
+  }
   return (
     <div>
       <h1>Pokemon List</h1>
       <ul>
         {pokemon.map((p) => (
-          <li key={p.name}>{p.name}</li>
+          <>
+            <li key={p.name}>{p.name}</li>
+            <img src={p.sprites.front_default} alt={p.name} />
+          </>
         ))}
       </ul>
     </div>
--- src/api/PokeRequests.ts
+++ src/api/PokeRequests.ts
@@ -3,6 +3,9 @@ import axios from "axios";
 interface Pokemon {
     name: string;
     url: string;
+    sprites: {
+        front_default: string;
+    };
 }
 
 interface PokemonResponse {
@@ -10,12 +13,16 @@ interface PokemonResponse {
 }
 
 const API_URL = 'https://pokeapi.co/api/v2';
+// https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/132.png
 
 async function getAllPokemon() {
     const response = await axios.get<PokemonResponse>(`${API_URL}/pokemon`);
     return response.data.results;
 }
 
-// https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/132.png
+// async function getPokeImage() {
+//     const response = await axios.get<PokemonResponse>(``)
+// }
+
 
 export default getAllPokemon;
